81f90b65edb1478d690268ded1bf3f41994c3966
sachitnjk <sachitnjk@gmail.com>
2024-12-03 19:50:20 +0530

    <Movement <Adding Acceleration Decceleration> WIP>

--- Assets/Scripts/TankRelated/TankMove.cs
+++ Assets/Scripts/TankRelated/TankMove.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class TankMove : MonoBehaviour
 {
@@ -12,13 +13,18 @@ public class TankMove : MonoBehaviour
 
     private Vector2 moveInput;
     private float turnInput;
-    
+
+    private bool isMoving = false;
+    private bool isTurning = false;
+        
     [Header("Tank References")]
     [SerializeField] private Rigidbody tankRB;
     
+    [FormerlySerializedAs("moveSpeed")]
     [Header("Tank Attributes")]
+    [SerializeField] private float currentMoveSpeed;
     [Range(1f, 10f)]
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float maxSpeed = 10f;
     private void Start()
     {
         Initialize();
@@ -53,18 +59,75 @@ public class TankMove : MonoBehaviour
             moveAction = playerInput.actions["Movement"];
             turnAction = playerInput.actions["TurretRotation"];
         }
+
+        currentMoveSpeed = 0f;
     }
     
     private void MoveTank()
     {
         moveInput = moveAction.ReadValue<Vector2>();
+        currentMoveSpeed = Mathf.Clamp(currentMoveSpeed, 0f, maxSpeed);
+
+        if (GetMovingStatus())
+        {
+            HandleAccelerateDecelerateOnMove();
+        }
+        else if(!moveAction.IsPressed())
+        {
+            HandleDeccelerate();
+        }
+        else if (!isMoving && moveAction.IsPressed())
+        {
+            currentMoveSpeed++;
+            isMoving = true;
+        }
 
         if (moveInput != null)
         {
-            Vector3 forwardMovement = transform.forward * moveInput.y * moveSpeed * Time.fixedDeltaTime;
+            Vector3 forwardMovement = transform.forward * moveInput.y * currentMoveSpeed * Time.fixedDeltaTime;
             
             tankRB.MovePosition(tankRB.position + forwardMovement);
         }
+    }
+
+    private void HandleAccelerateDecelerateOnMove()
+    {
+        
+        if (moveAction.IsPressed())
+        {
+            if (currentMoveSpeed < maxSpeed)
+            {
+                currentMoveSpeed++;
+            }
+        }
+        else if(!moveAction.IsPressed())
+        {
+            if (currentMoveSpeed > 0f)
+            {
+                currentMoveSpeed--;
+            }
+        }
+    }
+
+    private void HandleDeccelerate()
+    {
+        if (currentMoveSpeed > 0f)
+        {
+            currentMoveSpeed--;
+        }
+    }
+
+    private bool GetMovingStatus()
+    {
+        if (tankRB.velocity.magnitude > 0.1f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        return isMoving;
     }
 
     private void TurnTurret()
